805c2acf99973bffb3eecd2142517a67a5a2d5b3
Hannah Kim <hannahsohkim@gmail.com>
2019-03-18 12:53:00 -0700

    Add event listener for clicks

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,9 +1,27 @@
-// document.getElementsByClassName('box').addEventListener('click', function(event) {
-//   console.log('X')
-// })
 
-// document.getElementsByClassName('box').onClick = alert('hi');
 
-document.addEventListener('click', function() {
-  document.getElementsByClassName('box').innerHTML = 'X'
-})
+const boxes = document.getElementsByClassName('box');
+
+for (let i = 0; i < boxes.length; i++) {
+  boxes[i].addEventListener('click', addMove)
+  }
+
+
+function addMove (event) {
+  const player1 = true;
+  const player2 = false;
+
+  for (let i = 1; i <= 9; i++) {
+    if (player1 === true && player2 === false) {
+      if (event.target.innerHTML.length === 0) {
+        event.target.innerHTML = 'X';
+        player1 = false;
+        player2 = true;
+        i++;
+      } else {
+        event.target.innerHTML = 'O';
+        i++;
+      }
+    }
+  }
+}
