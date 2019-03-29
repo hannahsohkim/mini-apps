d079b462b8cd267d2dc1cafc918e6ab5e0316301
Hannah Kim <hannahsohkim@gmail.com>
2019-03-28 21:41:59 -0700

    Transfer code over from private repo

--- csv-report-generator/server.js
+++ csv-report-generator/server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
-app.use(express.static('client'))
+const bodyParser = require('body-parser');
+const fs = require('fs')
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('client'));
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
 app.post('/', (req, res) => {
-  console.log(req.body, 'REQ BODY')
-  res.render('index.html')
+  console.log(req.body.data, 'REQ BODY')
+  var body = req.body.data;
+  var obj = JSON.parse(body);
+  var props = [obj.properties]
+  console.log(props, 'PROPS')
+  var headers = 'firstName,lastName,county,city,role,sales';
+  var output = headers + '\r\n';
+  for (var key in props[0]) {
+    output += '"' + key + '",' + props[key] + '\r\n';
+  }
+  res.render('index', {data: output});
+  // res.sendFile('index.html', {'root': 'client'})
 })
+
 
 
 app.listen(3000, () => {
